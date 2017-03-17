c2c760c930b877f4a0cf847049e53f1533aa1ef0
Michal Porubsky <mikx@ksp.sk>
2017-03-17 19:51:24 +0100

    Initial repozitory - script that extracts relevant info

---
+++ main.py
@@ -0,0 +1,19 @@
+import csv
+
+what = 'účastník'
+# what = 'náhradník'
+
+# zena = True
+zena = False
+
+B = [ x for x in open('baby.data').read().splitlines() ]
+
+A = [
+	( x['user__first_name'] + ' ' + x['user__last_name'], x['user__email'] )
+	for x in csv.DictReader(open('inv.data'))
+	if x['type'] == what and not (zena ^ (x['user__first_name'] in B))
+]
+
+# print('\n'.join([', '.join(x) for x in A])) # meno, email
+# print('\n'.join([x[0] for x in A])) # meno
+print(', '.join([x[1] for x in A])) # email, email, ...
