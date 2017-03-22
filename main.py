592b9c9a338e5cebe4c3f0ac57c7c31009803a80
Michal Porubsky <mikx@ksp.sk>
2017-03-22 23:05:55 +0100

    Better output.

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import csv
 
-what = 'účastník'
-# what = 'náhradník'
-
-# baba = True
-baba = False
+whats = ['účastník', 'náhradník']
+babas = [True, False]
 
 B = [ x for x in open('baby.data').read().splitlines() ]
 
-A = [
-	( x['user__first_name'] + ' ' + x['user__last_name'], x['user__email'] )
-	for x in csv.DictReader(open('data'))
-	if x['type'] == what and not (baba ^ (x['user__first_name'] in B))
-]
+for what in whats:
+	for baba in babas:
+		A = [
+			( x['user__first_name'] + ' ' + x['user__last_name'], x['user__email'] )
+			for x in csv.DictReader(open('data'))
+			if x['type'] == what and not (baba ^ (x['user__first_name'] in B))
+		]
 
-# print('\n'.join([', '.join(x) for x in A])) # meno, email
-# print('\n'.join([x[0] for x in A])) # meno
-print(', '.join([x[1] for x in A])) # email, email, ...
+		print(what + ' (' + ('baba' if baba else 'chalan') + '):')
+		# print('\n'.join([', '.join(x) for x in A])) # meno, email
+		# print('\n'.join([x[0] for x in A])) # meno
+		print('\t' + ', '.join([x[1] for x in A])) # email, email, ...
+		print()
